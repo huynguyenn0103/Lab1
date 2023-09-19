a2d78541cdd89645875c229e001aea6a4efe458c
huynguyenn0103 <huynguyenn0103@gmail.com>
2023-09-19 13:34:33 +0700

    second commit ex10

--- src/Core/Src/main.c
+++ src/Core/Src/main.c
@@ -185,44 +185,11 @@ static void MX_GPIO_Init(void)
 }
 
 /* USER CODE BEGIN 4 */
+int led_pins[] = {LED_0_Pin, LED_1_Pin, LED_2_Pin, LED_3_Pin, LED_4_Pin, LED_5_Pin, LED_6_Pin, LED_7_Pin, LED_8_Pin, LED_9_Pin, LED_10_Pin, LED_11_Pin};
 void setNumberOnClock(int num){
-	if(num == 0){
-		  HAL_GPIO_WritePin(LED_0_GPIO_Port, LED_0_Pin, GPIO_PIN_RESET);
-	}
-	else if (num == 1){
-		  HAL_GPIO_WritePin(LED_1_GPIO_Port, LED_1_Pin, GPIO_PIN_RESET);
-	}
-	else if (num == 2){
-		  HAL_GPIO_WritePin(LED_2_GPIO_Port, LED_2_Pin, GPIO_PIN_RESET);
-	}
-	else if (num == 3){
-		  HAL_GPIO_WritePin(LED_3_GPIO_Port, LED_3_Pin, GPIO_PIN_RESET);
-	}
-	else if (num == 4){
-		  HAL_GPIO_WritePin(LED_4_GPIO_Port, LED_4_Pin, GPIO_PIN_RESET);
-	}
-	else if (num == 5){
-		  HAL_GPIO_WritePin(LED_5_GPIO_Port, LED_5_Pin, GPIO_PIN_RESET);
-	}
-	else if (num == 6){
-		  HAL_GPIO_WritePin(LED_6_GPIO_Port, LED_6_Pin, GPIO_PIN_RESET);
-	}
-	else if (num == 7){
-		  HAL_GPIO_WritePin(LED_7_GPIO_Port, LED_7_Pin, GPIO_PIN_RESET);
-	}
-	else if (num == 8){
-		  HAL_GPIO_WritePin(LED_8_GPIO_Port, LED_8_Pin, GPIO_PIN_RESET);
-	}
-	else if (num == 9){
-		  HAL_GPIO_WritePin(LED_9_GPIO_Port, LED_9_Pin, GPIO_PIN_RESET);
-	}
-	else if (num == 10){
-		  HAL_GPIO_WritePin(LED_10_GPIO_Port, LED_10_Pin, GPIO_PIN_RESET);
-	}
-	else if(num == 11){
-		  HAL_GPIO_WritePin(LED_11_GPIO_Port, LED_11_Pin, GPIO_PIN_RESET);
-	}
-
+    if (num >= 0 && num <= 11) {
+        HAL_GPIO_WritePin(GPIOA, led_pins[num], GPIO_PIN_RESET);
+    }
 }
 void clearAllClock(void){
 	HAL_GPIO_WritePin(GPIOA, LED_0_Pin|LED_1_Pin|LED_2_Pin|LED_3_Pin
